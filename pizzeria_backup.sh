dad33204126fa1cb462ca5450481564942d5022a
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 23:02:04 -0400

    use -d operator to find directory in if statement and reorder vars for in-order parsing

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 
 SRC_DIR="/tmp/backups"
-DEST_DIR_CHECK=$(ls -al /tmp/ | grep "$DEST_DIR")
 DEST_DIR="updated_menu"
+DEST_DIR_CHECK=$(ls -al /tmp/ | grep "$DEST_DIR")
 DATE=$(date +%Y-%m-%d)
 
-if [[ -z $DEST_DIR_CHECK ]]; then
+if [[ ! -d "/tmp/$DEST_DIR" ]]; then
     mkdir -p "/tmp/$DEST_DIR"
     echo "New backup directory created named: /tmp/$DEST_DIR"
 else
     echo "Backup directory already exists named /tmp/$DEST_DIR"
 fi
-
