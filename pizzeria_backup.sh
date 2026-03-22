20ce7a6037e9b3bce5280ffa46394e1ae5f62778
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 22:58:23 -0400

    add more brackets for if statement

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -5,7 +5,7 @@ DEST_DIR_CHECK=$(ls -al /tmp/ | grep "$DEST_DIR")
 DEST_DIR="updated_menu"
 DATE=$(date +%Y-%m-%d)
 
-if [ -z $DEST_DIR_CHECK ]; then
+if [[ -z $DEST_DIR_CHECK ]]; then
     mkdir -p "/tmp/$DEST_DIR"
     echo "New backup directory created named: /tmp/$DEST_DIR"
 else
