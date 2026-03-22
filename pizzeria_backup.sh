005b5facdd663e18b07e5e90fd4f9442f778ec69
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 22:22:42 -0400

    Add if else statement for script

---
+++ pizzeria_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+SRC_DIR="/tmp/backups"
+DEST_DIR="/tmp/updated_menu_backups"
+DATE=$(date +%Y-%m-%d)
+
+if [ -z "$DEST_DIR" ]; then
+    mkdir -p $DEST_DIR
+    echo "New backup directory created named: $DEST_DIR"
+else
+    echo "Backup directory already exists named $DEST_DIR"
+fi
+
