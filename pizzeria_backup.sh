acfbb278c88d0b2ce2ff891176615fbcc9e44e78
Adonis Bashi <adonisbashi@gmail.com>
2026-03-22 16:59:47 -0400

    Add tar backup section

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -2,6 +2,7 @@
 
 SRC_DIR="/tmp/backups"
 DEST_DIR="/tmp/updated_menu"
+TAR_BACKUP="archive.tar.gz"
 DATE=$(date +%Y-%m-%d)
 
 if [[ ! -d "$DEST_DIR" ]]; then
@@ -9,4 +10,11 @@ if [[ ! -d "$DEST_DIR" ]]; then
     echo "New backup directory created named: $DEST_DIR"
 else
     echo "Backup directory already exists named $DEST_DIR"
+fi
+
+if [[ -d "$SRC_DIR" ]]; then
+    tar -czvf "$TAR_BACKUP" "$SRC_DIR"
+    echo "Created archive: $TAR_BACKUP"
+else
+    echo "Error: directory '$SRC_DIR' not found!"
 fi
