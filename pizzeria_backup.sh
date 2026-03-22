b18f11ebc92ceef130a0440de388437114400a97
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 22:46:22 -0400

    change check to -z

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -5,7 +5,7 @@ DEST_DIR_CHECK=$( ls /tmp | grep $DEST_DIR)
 DEST_DIR="updated_menu"
 DATE=$(date +%Y-%m-%d)
 
-if [ $DEST_DIR_CHECK -eq "" ]; then
+if [ -z $DEST_DIR_CHECK ]; then
     mkdir -p "/tmp/$DEST_DIR"
     echo "New backup directory created named: /tmp/$DEST_DIR"
 else
