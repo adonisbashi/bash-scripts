5c13d3584a939ac001def2d21ed42be810077344
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 22:29:02 -0400

    another edit to if

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -4,7 +4,7 @@ SRC_DIR="/tmp/backups"
 DEST_DIR="/tmp/updated_menu_backups"
 DATE=$(date +%Y-%m-%d)
 
-if [ "$DEST_DIR" != "$DEST_DIR" ]; then
+if [ "$DEST_DIR" != "/tmp/updated_menu_backups" ]; then
     mkdir -p $DEST_DIR
     echo "New backup directory created named: $DEST_DIR"
 else
