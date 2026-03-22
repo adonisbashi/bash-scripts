2401fbab31c8e5383b9412b58255e3603aabe1dc
Adonis Bashi <adonisbashi@gmail.com>
2026-03-21 22:44:32 -0400

    change check to eq instead of =

--- pizzeria_backup.sh
+++ pizzeria_backup.sh
@@ -5,7 +5,7 @@ DEST_DIR_CHECK=$( ls /tmp | grep $DEST_DIR)
 DEST_DIR="updated_menu"
 DATE=$(date +%Y-%m-%d)
 
-if [ $DEST_DIR_CHECK = "" ]; then
+if [ $DEST_DIR_CHECK -eq "" ]; then
     mkdir -p "/tmp/$DEST_DIR"
     echo "New backup directory created named: /tmp/$DEST_DIR"
 else
